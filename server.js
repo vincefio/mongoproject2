a07d5ea5a66fd0b3919f3f1769d7656db38c4e7e
Vincent Fiorilli <Vincent Fiorilli>
2017-08-01 10:47:22 -0400

    add process.env to port

--- server.js
+++ server.js
@@ -310,6 +310,6 @@ console.log("working a little");
 
 
 // Listen on port 3000
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("App running on port 3000!");
 });
